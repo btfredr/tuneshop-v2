f39901ed902a0ddcaab41539b32480f9a2e863c9
Fredrik <fredrikf2000@gmail.com>
2021-06-06 14:05:54 +0200

    added total price to cart

--- src/components/content/CartGrid.js
+++ src/components/content/CartGrid.js
@@ -1,6 +1,10 @@
 import CartItem from "./CartItem";
 
 export const CartGrid = ({ products, cartItems }) => {
+  const getTotal = () => {
+    return cartItems.reduce((sum, { price }) => sum + price, 0);
+  };
+
   if (cartItems.length === 0) {
     return <h1>Cart is empty.</h1>;
   } else {
@@ -9,6 +13,8 @@ export const CartGrid = ({ products, cartItems }) => {
         {products.map((product) => (
           <CartItem key={product.id} product={product}></CartItem>
         ))}
+
+        <div>Total Cost: ${getTotal()}</div>
       </section>
     );
   }
